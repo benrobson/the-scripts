e0bbbf8048b297c24f8ed18d1e80d096617e8711
benrobson <benrobson76@gmail.com>
2021-11-11 10:34:51 +1100

    Deploy updates.

--- DownloadApplicationPackage.bat
+++ DownloadApplicationPackage.bat
@@ -1,7 +1,7 @@
 @REM Download Application Setup
-wget --no-check-certificate --content-disposition https://gist.githubusercontent.com/benrobson/776ff7cc8f30810bfa06aa2fd29e4638/raw/b5f78a202f5e1dd19eb46995b13ab679fae2d76f/ApplicationSetup.bat
-curl -LJO https://gist.githubusercontent.com/benrobson/776ff7cc8f30810bfa06aa2fd29e4638/raw/b5f78a202f5e1dd19eb46995b13ab679fae2d76f/ApplicationSetup.bat
+wget --no-check-certificate --content-disposition https://github.com/benrobson/the-scripts/raw/main/ApplicationSetup.bat
+curl -LJO https://github.com/benrobson/the-scripts/raw/main/ApplicationSetup.bat
 
 @REM Download Personalise Computer
-wget --no-check-certificate --content-disposition https://gist.githubusercontent.com/benrobson/723a98a50e2b04adb34d6063c33d1fb4/raw/de04ab30547ce65c9942b6a0f3b136ece2ab162a/PersonaliseComputer.bat
-curl -LJO https://gist.githubusercontent.com/benrobson/723a98a50e2b04adb34d6063c33d1fb4/raw/de04ab30547ce65c9942b6a0f3b136ece2ab162a/PersonaliseComputer.bat
+wget --no-check-certificate --content-disposition https://github.com/benrobson/the-scripts/raw/main/PersonaliseComputer.bat
+curl -LJO https://github.com/benrobson/the-scripts/raw/main/PersonaliseComputer.bat
--- README.md
+++ README.md
@@ -1,2 +1,35 @@
-# the-scripts
-Batch and PowerShell Scripts.
+# The Scripts
+A collection of Batch and PowerShell scripts used for various purposes.
+
+## DownloadApplicationPackage.bat
+### Function
+Downloads the PersonaliseComputer.bat and ApplicationSetup.bat packages together, easily used to auto-update, download and deploy easily.
+
+<hr>
+
+## ApplicationSetup.bat
+### Function
+A script used to automatically personalise a computer to deploy for clients.
+
+### Features
+* Install TeamViewer
+* Install Adobe Reader
+* Install Google Chrome
+
+<hr>
+
+## PersonaliseComputer.bat
+### Function
+A script used to automatically personalise a computer to deploy for clients.
+
+### Features
+* Hides Cortana Button.
+* Hides Task View Button.
+* Hides People Button.
+* Removes News and Interests from Taskbar.
+* Clear all Taskbar items.
+* Adds This PC icon to Desktop.
+
+### To Do
+* Removes all Start Menu Icons/Tiles.
+* Add User's Files to Desktop.
